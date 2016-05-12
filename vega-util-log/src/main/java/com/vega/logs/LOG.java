a8773d10134e5b481583453c0639778e4c58ea26
wangyongbing <shuijingvega@gmail.com>
2016-05-12 19:49:54 +0800

    log输出支持mq和logback初始

--- vega-util-log/src/main/java/com/vega/logs/LOG.java
+++ vega-util-log/src/main/java/com/vega/logs/LOG.java
@@ -2,324 +2,39 @@ package com.vega.logs;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.Marker;
+
+import com.vega.logs.logutils.LOGForLogback;
+import com.vega.logs.logutils.LOGForMQ;
 
 /**
  * 
  * 统一的日志工具类
  * 后续补充功能
  * 1.支持mq
- * 2.支持精确的级别控制
+ * 2.支持logback
  * 
  * @author wangyongbing
  *
  */
-public class LOG implements Logger {
-	public Logger logger = LoggerFactory.getLogger(getClass());
+public class LOG {
 
-	@Override
-	public String getName() {
-		return logger.getName();
+	/**
+	 * 通过logback输出日志
+	 * @param clazz
+	 * @return
+	 */
+	public Logger getLOG(Class<?> clazz) {
+		return LOGForLogback.getLogger(clazz);
 	}
 
-	@Override
-	public boolean isTraceEnabled() {
-		return logger.isTraceEnabled();
+	/**
+	 * 通过MQ输出日志
+	 * 
+	 * @param clazz
+	 * @param queueName
+	 * @return
+	 */
+	public Logger getLOG(Class<?> clazz, String clientId) {
+		return LOGForMQ.getLogger(clazz, clientId);
 	}
-
-	@Override
-	public void trace(String msg) {
-		logger.trace(msg);
-	}
-
-	@Override
-	public void trace(String format, Object arg) {
-		logger.trace(format, arg);
-	}
-
-	@Override
-	public void trace(String format, Object arg1, Object arg2) {
-		logger.trace(format, arg1, arg2);
-	}
-
-	@Override
-	public void trace(String format, Object... arguments) {
-		logger.trace(format, arguments);
-	}
-
-	@Override
-	public void trace(String msg, Throwable t) {
-		logger.trace(msg, t);
-	}
-
-	@Override
-	public boolean isTraceEnabled(Marker marker) {
-		return logger.isTraceEnabled(marker);
-	}
-
-	@Override
-	public void trace(Marker marker, String msg) {
-		logger.trace(marker, msg);
-	}
-
-	@Override
-	public void trace(Marker marker, String format, Object arg) {
-		logger.trace(marker, format, arg);
-	}
-
-	@Override
-	public void trace(Marker marker, String format, Object arg1, Object arg2) {
-		logger.trace(marker, format, arg1, arg2);
-	}
-
-	@Override
-	public void trace(Marker marker, String format, Object... argArray) {
-		logger.trace(marker, format, argArray);
-	}
-
-	@Override
-	public void trace(Marker marker, String msg, Throwable t) {
-		logger.trace(marker, msg, t);
-	}
-
-	@Override
-	public boolean isDebugEnabled() {
-		return logger.isDebugEnabled();
-	}
-
-	@Override
-	public void debug(String msg) {
-		logger.debug(msg);
-	}
-
-	@Override
-	public void debug(String format, Object arg) {
-		logger.debug(format, arg);
-	}
-
-	@Override
-	public void debug(String format, Object arg1, Object arg2) {
-		logger.debug(format, arg1, arg2);
-	}
-
-	@Override
-	public void debug(String format, Object... arguments) {
-		logger.debug(format, arguments);
-	}
-
-	@Override
-	public void debug(String msg, Throwable t) {
-		logger.debug(msg, t);
-	}
-
-	@Override
-	public boolean isDebugEnabled(Marker marker) {
-		return logger.isDebugEnabled(marker);
-	}
-
-	@Override
-	public void debug(Marker marker, String msg) {
-		logger.debug(marker, msg);
-	}
-
-	@Override
-	public void debug(Marker marker, String format, Object arg) {
-		logger.debug(marker, format, arg);
-	}
-
-	@Override
-	public void debug(Marker marker, String format, Object arg1, Object arg2) {
-		logger.debug(marker, format, arg1, arg2);
-	}
-
-	@Override
-	public void debug(Marker marker, String format, Object... arguments) {
-		logger.debug(marker, format, arguments);
-	}
-
-	@Override
-	public void debug(Marker marker, String msg, Throwable t) {
-		logger.debug(marker, msg, t);
-	}
-
-	@Override
-	public boolean isInfoEnabled() {
-		return logger.isTraceEnabled();
-	}
-
-	@Override
-	public void info(String msg) {
-		logger.info(msg);
-	}
-
-	@Override
-	public void info(String format, Object arg) {
-		logger.info(format, arg);
-	}
-
-	@Override
-	public void info(String format, Object arg1, Object arg2) {
-		logger.info(format, arg1, arg2);
-	}
-
-	@Override
-	public void info(String format, Object... arguments) {
-		logger.info(format, arguments);
-	}
-
-	@Override
-	public void info(String msg, Throwable t) {
-		logger.info(msg, t);
-	}
-
-	@Override
-	public boolean isInfoEnabled(Marker marker) {
-		return logger.isInfoEnabled(marker);
-	}
-
-	@Override
-	public void info(Marker marker, String msg) {
-		logger.info(marker, msg);
-	}
-
-	@Override
-	public void info(Marker marker, String format, Object arg) {
-		logger.info(marker, format, arg);
-	}
-
-	@Override
-	public void info(Marker marker, String format, Object arg1, Object arg2) {
-		logger.info(marker, format, arg1, arg2);
-	}
-
-	@Override
-	public void info(Marker marker, String format, Object... arguments) {
-		logger.info(marker, format, arguments);
-	}
-
-	@Override
-	public void info(Marker marker, String msg, Throwable t) {
-		logger.info(marker, msg, t);
-	}
-
-	@Override
-	public boolean isWarnEnabled() {
-		return logger.isWarnEnabled();
-	}
-
-	@Override
-	public void warn(String msg) {
-		logger.warn(msg);
-	}
-
-	@Override
-	public void warn(String format, Object arg) {
-		logger.warn(format, arg);
-	}
-
-	@Override
-	public void warn(String format, Object... arguments) {
-		logger.warn(format, arguments);
-	}
-
-	@Override
-	public void warn(String format, Object arg1, Object arg2) {
-		logger.warn(format, arg1, arg2);
-	}
-
-	@Override
-	public void warn(String msg, Throwable t) {
-		logger.warn(msg, t);
-	}
-
-	@Override
-	public boolean isWarnEnabled(Marker marker) {
-		return logger.isWarnEnabled(marker);
-	}
-
-	@Override
-	public void warn(Marker marker, String msg) {
-		logger.warn(marker, msg);
-	}
-
-	@Override
-	public void warn(Marker marker, String format, Object arg) {
-		logger.warn(marker, format, arg);
-	}
-
-	@Override
-	public void warn(Marker marker, String format, Object arg1, Object arg2) {
-		logger.warn(marker, format, arg1, arg2);
-	}
-
-	@Override
-	public void warn(Marker marker, String format, Object... arguments) {
-		logger.warn(marker, format, arguments);
-	}
-
-	@Override
-	public void warn(Marker marker, String msg, Throwable t) {
-		logger.warn(marker, msg, t);
-	}
-
-	@Override
-	public boolean isErrorEnabled() {
-		return logger.isErrorEnabled();
-	}
-
-	@Override
-	public void error(String msg) {
-		logger.error(msg);
-	}
-
-	@Override
-	public void error(String format, Object arg) {
-		logger.error(format, arg);
-	}
-
-	@Override
-	public void error(String format, Object arg1, Object arg2) {
-		logger.error(format, arg1, arg2);
-	}
-
-	@Override
-	public void error(String format, Object... arguments) {
-		logger.error(format, arguments);
-	}
-
-	@Override
-	public void error(String msg, Throwable t) {
-		logger.error(msg, t);
-	}
-
-	@Override
-	public boolean isErrorEnabled(Marker marker) {
-		return logger.isErrorEnabled(marker);
-	}
-
-	@Override
-	public void error(Marker marker, String msg) {
-		logger.error(marker, msg);
-	}
-
-	@Override
-	public void error(Marker marker, String format, Object arg) {
-		logger.error(marker, format, arg);
-	}
-
-	@Override
-	public void error(Marker marker, String format, Object arg1, Object arg2) {
-		logger.error(marker, format, arg1, arg2);
-	}
-
-	@Override
-	public void error(Marker marker, String format, Object... arguments) {
-		logger.error(marker, format, arguments);
-	}
-
-	@Override
-	public void error(Marker marker, String msg, Throwable t) {
-		logger.error(marker, msg, t);
-	}
-
 }
---
+++ vega-util-log/src/main/java/com/vega/logs/constants/LOGConstant.java
@@ -0,0 +1,14 @@
+package com.vega.logs.constants;
+
+public interface LOGConstant {
+
+	/**
+	 * 通过mq处理日志
+	 */
+	String LOG_MQ = "1";
+	
+	/**
+	 * 通过logback处理日志
+	 */
+	String LOG_LOGBACK = "0";
+}
